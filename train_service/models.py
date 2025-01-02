31efa23e3f2912297b9e3c5e1cd5d41834b31f3a
Kateryna <kat.rin47u@gmail.com>
2025-01-02 18:25:34 +0000

    feat: Add Route model

--- train_service/models.py
+++ train_service/models.py
@@ -7,3 +7,20 @@ class Station(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.city})"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        "Station",
+        on_delete=models.CASCADE,
+        related_name="source_routes",
+    )
+    destination = models.ForeignKey(
+        "Station",
+        on_delete=models.CASCADE,
+        related_name="destination_routes",
+    )
+    distance = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.source.name} - {self.destination.name}"
