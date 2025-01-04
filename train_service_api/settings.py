38ab2a50b98e3d2dcf1894788b3b65df8c0dc4cb
Kateryna <kat.rin47u@gmail.com>
2025-01-04 01:52:44 +0000

    chore(train_service_api): Configure REST framework settings

--- train_service_api/settings.py
+++ train_service_api/settings.py
@@ -132,3 +132,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
