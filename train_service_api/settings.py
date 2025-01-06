66d7159f4679b12769afed2275f4a5511ff2a966
Kateryna <kat.rin47u@gmail.com>
2025-01-06 16:38:15 +0000

    feat: Add throttle settings

--- train_service_api/settings.py
+++ train_service_api/settings.py
@@ -148,6 +148,11 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "30/day", "user": "100/day"},
 }
 
 SIMPLE_JWT = {
